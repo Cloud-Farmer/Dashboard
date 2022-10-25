11ca072ba949dcb3853c5937098a5e7d143fd77d
Dark-jin <chch4lee1@naver.com>
2022-10-25 22:06:36 +0900

    feat: areachart&linechart toggle feat: sensor Card & control toggle feat: control toggle on/off 통신 feat: axios 통신

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -8,53 +8,38 @@ import {
   Toggle,
   ToggleItem,
 } from '@tremor/react';
-import TestChart1 from '../Example/TestChart1';
-import TextChart2 from '../Example/TextChart2';
-import TestTable from '../Example/TestTable';
-import { useLanguage } from '../../hooks';
+//import TestChart1 from '../Example/TestChart1';
+//import TextChart2 from '../Example/TextChart2';
+//import TestTable from '../Example/TestTable';
+//import { useLanguage } from '../../hooks';
+import SensorCard from '../SensorCard';
+import Control from '../Control';
+import Chart from '../Chart';
 
 const Main = () => {
-  const [lang, setLang] = useLanguage();
+  // const [lang, setLang] = useLanguage();
   return (
     <>
-      <Toggle
+      {/* <Toggle
         color="zinc"
         defaultValue={lang}
         handleSelect={(value: 'en' | 'ko') => setLang(value)}
-      >
-        <ToggleItem value="en" text="ENGLISH" />
-        <ToggleItem value="ko" text="KOREAN" />
-      </Toggle>{' '}
+      > */}
+      {/* <ToggleItem value="en" text="ENGLISH" />
+        <ToggleItem value="ko" text="KOREAN" /> */}
+      {/* </Toggle>{' '} */}
       <ColGrid
         numCols={1}
         numColsSm={2}
-        numColsLg={3}
+        numColsLg={4}
         gapX="gap-x-2"
         gapY="gap-y-2"
       >
-        <Col numColSpan={1} numColSpanLg={2}>
-          <TestChart1 />
+        <Col numColSpan={1} numColSpanLg={4}>
+          <Chart />
         </Col>
-        <Col>
-          <TextChart2 />
-        </Col>
-        <Col numColSpan={3} numColSpanLg={3}>
-          <TestTable />
-        </Col>
-        <Col>
-          <Card>
-            <Text>Title</Text>
-            <Metric>KPI 3</Metric>
-          </Card>
-        </Col>
-        <Card>
-          <Text>Title</Text>
-          <Metric>KPI 4</Metric>
-        </Card>
-        <Card>
-          <Text>Title</Text>
-          <Metric>KPI 5</Metric>
-        </Card>
+        <SensorCard />
+        <Control />
       </ColGrid>
     </>
   );
