520a753757727c7a4a881eba79dbb6d78a5216ea
Younggun Park <ygpark0922@gmail.com>
2022-11-23 14:45:30 +0900

    chore: ready for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
   assetsInclude: ['**/*.png'],
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: [
+      { find: '@src', replacement: resolve(__dirname, 'src') },
+      {
+        find: '@components',
+        replacement: resolve(__dirname, 'src/components'),
+      },
+    ],
+  },
   server: {
     proxy: {
       '/api': {
@@ -23,6 +32,6 @@ export default defineConfig({
       },
     },
     host: '0.0.0.0',
-    port: 3000,
+    port: 3001,
   },
 });
